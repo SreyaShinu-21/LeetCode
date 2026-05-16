b7a3410e0b655277ed6cd655a7ae9d99011a6936
Sreya Shinu <sreyashinu45@gmail.com>
2026-05-15 23:00:08 -0600

    Time: 72 ms (33.44%), Space: 107.5 MB (48.55%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -3,8 +3,9 @@ class Solution {
         int[] a=new int[t.length];
         Stack<Integer>s=new Stack<>();
         for(int i=0;i<t.length;i++){
-            while(!s.empty()&&t[i]>t[s.peek()])
+            while(!s.empty()&&t[i]>t[s.peek()]){
                 a[s.peek()]=i-s.pop();
+            }
             s.push(i);
         }
         return a;
