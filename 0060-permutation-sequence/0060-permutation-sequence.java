094401ceb584ff320e91ed0ef2a138cea1450c74
Sreya Shinu <sreyashinu45@gmail.com>
2026-09-02 14:11:09 +0530

    Time: 248 ms (10.73%), Space: 47.2 MB (6.18%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,27 +1,35 @@
 class Solution {
     public String getPermutation(int n, int k) {
-        String s="";
-        for (int i=1;i<=n; i++)
+        String s = "";
+        for (int i = 1; i <= n; i++) {
             s += i;
-        for (int i = 1; i < k; i++)
+        }
+        for (int i = 1; i < k; i++) {
             s = next(s);
+        }
         return s;
     }
-    String next(String s) {
-        char[] a = s.toCharArray();
-        int i = a.length - 2;
-        while (a[i] > a[i + 1])
+    String next(String s){
+        char[] a=s.toCharArray();
+        int i=a.length-2;
+        while (i>=0 && a[i]>=a[i+1]){
             i--;
-        int j = a.length - 1;
-        while (a[j] < a[i])
+        }
+        int j=a.length-1;
+        while(a[j]<=a[i]){
             j--;
-        char t = a[i];
-        a[i] = a[j];
-        a[j] = t;
-        for (i++, j = a.length - 1; i < j; i++, j--) {
-            t = a[i];
-            a[i] = a[j];
-            a[j] = t;
+        }
+        char temp=a[i];
+        a[i]=a[j];
+        a[j]=temp;
+        i++;
+        j=a.length-1;
+        while(i<j){
+            temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+            i++;
+            j--;
         }
         return new String(a);
     }
