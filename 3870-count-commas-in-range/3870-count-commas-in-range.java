b198eac1c178ebdc19124bf252079113d97c30dd
Sreya Shinu <sreyashinu45@gmail.com>
2026-09-08 13:59:28 +0530

    Time: 0 ms (100%), Space: 42.6 MB (39.85%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int countCommas(int n) {
+        if(n<1000){
+            return 0;
+        }
+        int diff=n-1000;
+        diff++;
+        return diff;
+    }
+}
